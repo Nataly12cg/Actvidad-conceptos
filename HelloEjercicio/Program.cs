c208db508ed4f42161435fd1bb8c3274593d5c1d
Nataly12cg <natalycorrea99@gmail.com>
2023-09-06 18:07:42 -0500

    Agregado ejercicio Root or Square

--- HelloEjercicio/Program.cs
+++ HelloEjercicio/Program.cs
@@ -58,7 +58,39 @@ namespace HelloEjercicio
                 Console.WriteLine("Entrada no válida para el primer número.");
             }
 
-            // Ejercicio 3: Otro ejercicio
+            // Ejercicio 3: Root or Square
+               Console.Write("Ingresa un número: ");
+               if (double.TryParse(Console.ReadLine(), out double numero3))
+            {
+               if (numero3 > 0)
+           {
+               double resultado3 = Math.Sqrt(numero3);
+               Console.WriteLine($"Resultado: {resultado3}");
+               }
+               else
+             {
+             double resultado3 = Math.Pow(numero3, 2);
+             Console.WriteLine($"Resultado: {resultado3}");
+              }
+              }
+             else
+             {
+             Console.WriteLine("Entrada no válida. Ingresa un número válido.");
+              }
+
+              // Ejercicio 4: Otro ejercicio
+             // Ejercicio 5: Otro ejercicio
+             // Ejercicio 6: Otro ejercicio
+             // Ejercicio 7: Otro ejercicio
+             // Ejercicio 8: Otro ejercicio
+             // Ejercicio 9: Otro ejercicio
+             // Ejercicio 10: Otro ejercicio
+             // Ejercicio 11: Otro ejercicio
+             // Ejercicio 12: Otro ejercicio
+             // Ejercicio 13: Otro ejercicio
+             // Ejercicio 14: Otro ejercicio
+             // Ejercicio 5: Otro ejercicio
+        
             
         }
     }
